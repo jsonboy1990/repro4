b0a4922bb2ae18b449217e67a734aa3c73564c5d
yehao <517324107@qq.com>
2020-10-09 11:41:14 +0800

    添加项目到本地仓库

---
+++ src/main/java/com/itheima/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("测试idea");
+        System.out.println("测试idea");
+        System.out.println("测试idea");
+    }
+}
